936598c6ba74e43666458a850ca83358a146084a
otiofrui <spam@dev.otiofrui.org>
2021-12-30 01:47:31 +0100

    adding code from sildes

---
+++ Block_1/slides_code.rb
@@ -0,0 +1,46 @@
+# Code aller Folien
+
+if x == 1
+    name = "eins"
+  elsif x == 2
+    name = "zwei"
+  elsif x == 3 then name = "drei"
+  elsif x == 4; name = "vier"
+  else
+    name = "viele"
+  end
+  
+
+name = if    x == 1 then "eins"
+elsif x == 2 then "zwei"
+elsif x == 3 then "drei"
+elsif x == 4 then "vier"
+else              "viele"
+end
+
+
+name = case
+when x == 1 then "eins"
+when x == 2 then "zwei"
+when x == 3 then "drei"
+when x == 4 then "vier"
+else "viele"
+end
+
+
+unless Bedingung
+    Code
+end
+
+unless Bedingung
+    Code
+else
+    Code
+end
+
+Code unless Bedingung
+
+
+def how_many_messages(n)
+    "Sie haben " + n.to_s + (n==1 ? " Nachricht." : " Nachrichten.")
+end
