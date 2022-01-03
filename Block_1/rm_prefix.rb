7cc11d938b42fa282c79c41a7ee9b12235b3e662
otiofrui <spam@dev.otiofrui.org>
2022-01-03 17:36:47 +0100

    change rm prefix

--- Block_1/rm_prefix.rb
+++ Block_1/rm_prefix.rb
@@ -1,9 +1,9 @@
 =begin
-Schreibe eine “Remove Prefix” Methode, die eine gegebene Anzahl an Buchstaben von einem Satz entfernt.
+Schreibe eine “Remove Prefix” Methode, die eine gegebene Anzahl an Buchstaben von einem Satzanfang entfernt.
 =end
 
-def rm_prefix(s, len=1)
-    s[len, s.length()]
+def rm_prefix(s, pre)
+    s[pre, s.length()]
 end
 
 puts rm_prefix("Error message: Please restart your computer!", 15)
