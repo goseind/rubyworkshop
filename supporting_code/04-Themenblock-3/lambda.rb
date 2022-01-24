258ce7c5fe8c6ee0a0f5c55714633c0c00571a28
Adrian Petschenka <adrian.petschenka@gmail.com>
2022-01-24 10:53:15 +0100

    added more exercices

--- supporting_code/04-Themenblock-3/lambda.rb
+++ supporting_code/04-Themenblock-3/lambda.rb
@@ -1,9 +1,43 @@
 =begin
-To pass arguments in the lambda function, we can either use normal lambda syntax or use the literal lambda operator ” -> ”
+Schreibe eine Hello-Funktion mit einem Lambda, das einen Parameter (Namen) übergeben bekommt. Wird kein Parameter übergeben, soll "Hello World" ausgegeben werden.
 =end
 
-# Using lambda keyword
-lambda_with_args = lambda {| s | puts "Hello "+ s }
+lambda_with_args = lambda {| s = "World" | puts "Hello "+ s }
 
-# Aufruf der Lambda Funktion mit Parameterübergabe
 lambda_with_args.call("Workshop")
+
+=begin
+Schreibe eine Funktion, die ein Lambda und einen Parameter Übergeben bekommt. Der Parameter wird an das Lambda übergeben und das Ergebnis soll ausgegeben werden.
+=end
+
+add_10 = lambda { |num| num + 10 }
+multiply_2 = lambda { |num| num * 2 }
+ 
+def using_lambda_with_functions(lambda, number)
+ 
+  puts lambda.call(number)
+ 
+end
+ 
+using_lambda_with_functions(add_10, 10)
+using_lambda_with_functions(multiply_2, 20)
+
+=begin
+Schreibe eine Funktion, die ein Lambda erstellt, das den Parameter überprüft und "OK" oder "N/A" zurückgibt.
+=end
+
+def build_lambda()​
+  my_hash = {}​
+  my_lambda = lambda do |key|​
+    if key == "200"
+      return "OK"​
+    else​
+      return "N/A"​
+    end
+  end
+  my_lambda​
+end​
+​
+code_check = build_lambda()​
+puts code_check["200"]    # => "OK"​
+puts code_check["404"]    # => "N/A"​
--- supporting_code/04-Themenblock-3/yield.rb
+++ supporting_code/04-Themenblock-3/yield.rb
@@ -1,5 +1,33 @@
 =begin
-Yield with argument: Below is the program of yield with argument.
+Schreibe ein Funktion, die ein 2 mal einen yield Aufruf macht, dazwischen soll eine weiterer String ausgegeben werden.
+=end
+
+def call_twice
+	puts "In the call_twice method"
+
+	# using yield keyword
+	yield
+	puts "Again from the call_twice method"
+	yield
+end
+
+call_twice {puts "This is a block"}
+
+=begin
+Schreibe eine Funktion die den Rückgabewert des yields ausgibt.
+=end
+
+def yield_with_return_value
+  hello_world = yield
+
+  puts hello_world
+end
+
+yield_with_return_value { "Hello World!" } # => Hello World!
+
+
+=begin
+Schreibe eine Funktion, die ein yield mit einem Parameter aufruft.
 =end
 
 def calc
@@ -11,3 +39,26 @@ end
 
 # Aufruf der Funktion mit yield block
 calc {|i| puts "calc #{i}"}
+
+=begin
+Beim Definieren einer Methode und innerhalb der Methode soll yield mit 2 Parametern aufgerufen werden.
+=end
+
+def arithmetic(a, b)
+	# yield mit 2 Parametern
+	yield(a, b)
+end
+
+# addition of two number
+puts "The sum of the two numbers is #{arithmetic(8, 2) { |a, b| a + b }}" 
+
+=begin
+Wenn yield aufgerufen wird, benötigt diese Methode einen block. Schreibe eine Funktion die sowohl mit, als auch ohne Parameter funktioniert.
+=end
+
+def optional_block
+  yield if block_given?
+end
+
+optional_block # => nil
+optional_block { puts 'optional block' }
