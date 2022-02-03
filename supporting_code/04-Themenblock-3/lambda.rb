94890f7fc7fa157e7b3fcc54b06a03c29176d6ff
Adrian Petschenka <adrian.petschenka@gmail.com>
2022-02-03 18:26:07 +0100

    added code to Themenblock3-2 (WIP)

--- supporting_code/04-Themenblock-3/lambda.rb
+++ supporting_code/04-Themenblock-3/lambda.rb
@@ -2,9 +2,12 @@
 Schreibe eine Hello-Funktion mit einem Lambda, das einen Parameter (Namen) übergeben bekommt. Wird kein Parameter übergeben, soll "Hello World" ausgegeben werden.
 =end
 
-lambda_with_args = lambda {| s = "World" | puts "Hello "+ s }
+lambda_with_args = lambda do |s = "World"| 
+	puts "Hello "+ s 
+end
 
-lambda_with_args.call("Workshop")
+lambda_with_args.call()    			# => "Hello World"​
+lambda_with_args.call("Workshop")	# => "Hello Workshop"​
 
 =begin
 Schreibe eine Funktion, die ein Lambda und einen Parameter Übergeben bekommt. Der Parameter wird an das Lambda übergeben und das Ergebnis soll ausgegeben werden.
@@ -14,20 +17,17 @@ add_10 = lambda { |num| num + 10 }
 multiply_2 = lambda { |num| num * 2 }
  
 def using_lambda_with_functions(lambda, number)
- 
   puts lambda.call(number)
- 
 end
  
-using_lambda_with_functions(add_10, 10)
-using_lambda_with_functions(multiply_2, 20)
+using_lambda_with_functions(add_10, 10)    		# => 20​
+using_lambda_with_functions(multiply_2, 20) 	# => 40​
 
 =begin
 Schreibe eine Funktion, die ein Lambda erstellt, das den Parameter überprüft und "OK" oder "N/A" zurückgibt.
 =end
 
 def build_lambda()​
-  my_hash = {}​
   my_lambda = lambda do |key|​
     if key == "200"
       return "OK"​
@@ -36,7 +36,7 @@ def build_lambda()​
     end
   end
   my_lambda​
-end​
+end
 ​
 code_check = build_lambda()​
 puts code_check["200"]    # => "OK"​
