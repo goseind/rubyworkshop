edd476b2bbd822068362cb7a17b9d39757449f82
otiofrui <domenic.gosein@stud.hs-mannheim.de>
2022-02-07 15:23:51 +0100

    Unterteilung in drei Notebooks

--- iruby_notebook/1-Unit/temp/rm_prefix.rb
+++
@@ -1,9 +0,0 @@
-=begin
-Schreibe eine “Remove Prefix” Methode, die eine gegebene Anzahl an Buchstaben von einem Satzanfang entfernt.
-=end
-
-def rm_prefix(s, pre)
-    s[pre, s.length()]
-end
-
-puts rm_prefix("Error message: Please restart your computer!", 15)
